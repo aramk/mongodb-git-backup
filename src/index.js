6fb2c1fa87248ff17cf5a378a40822740bb2277c
Aram Kocharyan <akarmenia@gmail.com>
2020-04-25 16:15:49 -0700

    Switch to using mongodump for better performance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-var yargs = require('yargs');
-var fs = require('fs');
-var path = require('path');
-var mongoBackup = require('mongodb-backup');
-var readdir = require('recursive-readdir');
-var del = require('del');
-var Q = require('q');
+const yargs = require('yargs');
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+const readdir = require('recursive-readdir');
+const del = require('del');
+const Q = require('q');
 
-var dir = yargs.argv.dir;
+let dir = yargs.argv.dir;
 if (!dir || !fs.lstatSync(dir).isDirectory()) {
   throw new Error('Directory not found');
 }
@@ -16,25 +16,27 @@ if (!fs.lstatSync(path.join(dir, '.git')).isDirectory()) {
 dir = path.resolve(__dirname, dir);
 log('Backing up MongoDB to directory:', dir);
 
-var uri = yargs.argv.uri;
+const uri = yargs.argv.uri;
 if (!uri) {
   throw new Error('No MongoDB URI provided');
 }
+console.log(`Parsed URI: ${JSON.stringify(getUriInfo(uri))}`);
+
+let runDf;
 
 // Allow running the processs immediately without scheduling.
 if (yargs.argv.now != null) {
   run();
 } else {
-  var CronJob = require('cron').CronJob;
+  const CronJob = require('cron').CronJob;
   // Run every hour by default.
-  var cronRange = yargs.argv.cron || '00 00 * * * *';
-  var job = new CronJob(cronRange, run,
+  const cronRange = yargs.argv.cron || '00 00 * * * *';
+  const job = new CronJob(cronRange, run,
     true, /* Start the job right now */
     yargs.argv.timezone || 'Australia/Melbourne'
   );
 }
 
-var runDf;
 function run() {
   // Prevent running if previous run is incomplete.
   if (runDf && runDf.promise && Q.isPending(runDf.promise)) return;
@@ -48,18 +50,15 @@ function run() {
 
 function backup() {
   log('Backing up data...');
-  var df = Q.defer();
-  mongoBackup({
-    uri: uri,
-    root: dir,
-    callback: function(err, result) {
-      if (err) {
-        log('Error during backup', err);
-        df.reject(err);
-      } else {
-        log('Backup successful', result);
-        df.resolve(result);
-      }
+  const df = Q.defer();
+  const {host, port, username, password, db} = getUriInfo(uri);
+  child_process.exec(`mongodump --host="${host}" --port="${port}" --username="${username}" --password="${password}" --db="${db}" --out=${dir}`, {}, (err, stdout, stderr) => {
+    if (err) {
+      log('Error during backup', err);
+      df.reject(err);
+    } else {
+      log('Backup successful', stdout);
+      df.resolve();
     }
   });
   return df.promise;
@@ -67,7 +66,7 @@ function backup() {
 
 function push() {
   log('Adding to Git...');
-  var df = Q.defer();
+  const df = Q.defer();
 
   function callback(err, result) {
     if (err) df.reject(err);
@@ -93,14 +92,14 @@ function push() {
 // Deletes the existing files in the subdirectores of the given directory.
 function deleteFiles(dir) {
   log('Deleting existing files...');
-  var df = Q.defer();
+  const df = Q.defer();
 
   // Exclude hidden directories (e.g. git).
-  var subdirs = getDirectories(dir).filter(function(directory) {
+  const subdirs = getDirectories(dir).filter(function(directory) {
     return !/^\./.test(directory);
   });
   log('Deleting directories:', subdirs);
-  var paths = subdirs.map(function(subdir) {
+  const paths = subdirs.map(function(subdir) {
     return path.join(dir, subdir, '**');
   });
   return Q.resolve(del(paths, {force: true}));
@@ -113,8 +112,24 @@ function getDirectories(srcpath) {
   });
 }
 
+function getUriInfo(uri) {
+  const RE_URI = /mongodb:\/\/(?:(\w+)(:[^@]+)?@)?(\w+)(:\d+)?\/([^\/]+)/;
+  const match = uri.match(RE_URI);
+  return {
+    host: match[3],
+    port: match[4] != null ? stripColonPrefix(match[4]) : '',
+    username: match[1] || '',
+    password: match[2] != null ? stripColonPrefix(match[2]) : '',
+    db: match[5],
+  }
+}
+
+function stripColonPrefix(str) {
+  return str.replace(/^:/, '');
+}
+
 function log() {
-  var args = Array.prototype.slice.call(arguments);
+  const args = Array.prototype.slice.call(arguments);
   args.unshift(new Date());
   console.log.call(console, args.join(' '));
 }
